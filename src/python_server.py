479df6aed58414d69cb8e31e6b1c6febc743aa60
Divya Poonthottam <divyapoonthottam@gmail.com>
2022-03-26 23:12:32 +0530

    minor spelling change

--- src/python_server.py
+++ src/python_server.py
@@ -2,7 +2,7 @@ import flask
 app=flask.Flask("my-python-server")
 @app.route("/")
 def title():
-    return "<html><head><title>Divya Project</title></head><body><h1>my server </body></html>"
+    return "<html><head><title>Divya's Project</title></head><body><h1>my server </body></html>"
 
 if __name__=="__main__":
     app.run(host = "0.0.0.0", port = 8080)
