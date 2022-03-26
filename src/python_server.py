87a7972377119e96ee9c1f24fee2a05959112566
Divya Poonthottam <divyapoonthottam@gmail.com>
2022-03-26 21:07:24 +0530

    Create a flask based python server

--- src/python_server.py
+++ src/python_server.py
@@ -1,2 +1,8 @@
 import flask
 app=flask.Flask("my-python-server")
+@app.route("/")
+def title():
+    return "<html><head><title>Divya Project</title></head><body><h1>my server </body></html>"
+
+if __name__=="__main__":
+    app.run(host = "0.0.0.0", port = 8080)
