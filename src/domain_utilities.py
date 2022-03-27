3e2a1781e49617e9dcb67ea7d20d62d9ab2c7fdf
Divya Poonthottam <divyapoonthottam@gmail.com>
2022-03-27 08:25:50 +0530

    Add function  to compare text-record with domain-text-record

--- src/domain_utilities.py
+++ src/domain_utilities.py
@@ -1,2 +1,15 @@
 import dns
 import dns.resolver
+def get_dns_record_form_domain(domain_name):
+    text_records=[]
+    answers=dns.resolver.resolve(domain_name,dns.rdatatype.RdataType.TXT)
+    for answer in answers:
+        text_record=str(answer).strip("\"")
+        text_records.append(text_record) 
+    return text_records   
+
+def search_for_text_record_in_domain(domain_name,text_record):
+    text_records_of_domain=get_dns_record_form_domain(domain_name)
+    if text_record in text_records_of_domain:
+        return text_record
+        
